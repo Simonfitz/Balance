a15b9258bc13143f19d32730e10f2cfef3939a71
daniel11235 <mccormd2@tcd.ie>
2025-04-16 21:59:58 +0100

    Added gameOptions file to allow for easier config

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
 import { Start } from './scenes/Start.js';
 import { Menu } from './scenes/Menu.js';
 import { Battleground } from './scenes/Battleground.js';
+import { GameOptions } from './gameConfig/gameOptions.js';
 
 const config = {
   type: Phaser.AUTO,
   title: 'Balance',
   description: '',
   parent: 'game-container',
-  width: 1280,
-  height: 720,
-  backgroundColor: '#000000',
+  width: GameOptions.gameSize.width,
+  height: GameOptions.gameSize.height,
+  backgroundColor: GameOptions.gameBackgroundColor,
   pixelArt: false,
-  scene: [Start, Menu, Battleground],
+  scene: [
+    Start,
+    Menu,
+    Battleground
+  ],
   scale: {
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.CENTER_BOTH
   },
-};
+}
 
 new Phaser.Game(config);
